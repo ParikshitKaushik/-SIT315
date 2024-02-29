3eb0ee2fa2326febc9b02fd10a359a2edd973557
ParikshitKaushik <122871202+ParikshitKaushik@users.noreply.github.com>
2024-02-29 11:51:27 +0530

    Task1.3C.ino

--- Module 1/Task1.3C.ino
+++ Module 1/Task1.3C.ino
@@ -1,12 +1,15 @@
 const int PIRSensorPin = 3; 
+const int ArduinoPin= 2;
 const int tmp36Pin = A0; 
 const int ledPin = 13;
+const int ArduinoLED = 10;
 volatile bool motionDetected = false;
 
 void setup() {
   pinMode(PIRSensorPin, INPUT);
   pinMode(ledPin, OUTPUT);
   attachInterrupt(digitalPinToInterrupt(PIRSensorPin), motionDetectedIsTrue, CHANGE);
+  attachInterrupt(digitalPinToInterrupt(ArduinoPin), Arduino, CHANGE);
   Serial.begin(9600);
 }
 
@@ -28,7 +31,12 @@ void loop() {
 void motionDetectedIsTrue() {
   motionDetected = true;
 }
-
+void Arduino(){
+	digitalWrite(ArduinoLED,HIGH);
+  	delay(1000);
+  	digitalWrite(ArduinoLED,LOW);
+  
+}
 float getTemperature() {
   int sensorValue = analogRead(tmp36Pin);
   float voltage = sensorValue * (5.0 / 1023.0);
