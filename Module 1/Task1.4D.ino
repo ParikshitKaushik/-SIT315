20ed0b7302b4fe17a2ee67576fec6738d7faf241
ParikshitKaushik <122871202+ParikshitKaushik@users.noreply.github.com>
2024-03-01 01:37:10 +0530

    Task1.4D.ino

--- Module 1/Task1.4D.ino
+++ Module 1/Task1.4D.ino
@@ -1,57 +1,59 @@
-const int PIRSensorPin = 3;
-const int tmp36Pin = A0;
-const int tiltSensorPin = 2;  
-const int ledPin = PB5;
-volatile bool motionDetected = false;
-volatile bool tiltDetected = false;
+#include <avr/io.h>
+#include <avr/interrupt.h>
 
-void attachPCINT(int pin, void (*function)()) {
-  pinMode(pin, INPUT);
-  digitalWrite(pin, HIGH); 
-  attachInterrupt(digitalPinToInterrupt(pin), function, CHANGE);
-}
+const int pirPin = 4;
+const int buttonPin = 5;
+int led1 = 13;
+int led2 = 11;
+int led3 = 10;
+volatile uint8_t pirState, buttonState;
+volatile uint8_t prepir, prebutton = 0;
+const uint16_t timer = 0;
+const uint16_t compare = 31250;
 
 void setup() {
-  pinMode(PIRSensorPin, INPUT);
-  pinMode(tiltSensorPin, INPUT);
-  DDRB != (1<<ledPin);
-  attachPCINT(PIRSensorPin, motionDetectedIsTrue);
-  attachPCINT(tiltSensorPin, tiltDetectedIsTrue);
   Serial.begin(9600);
+  pinMode(pirPin, INPUT);
+  pinMode(buttonPin, INPUT_PULLUP);
+  pinMode(led1, OUTPUT);
+  pinMode(led2, OUTPUT);
+  pinMode(led3, OUTPUT);
+
+  PCICR |= (1 << PCIE2);
+  PCMSK2 |= (1 << PCINT20) | (1 << PCINT21); 
+
+  TCCR1A = 0;
+  TCCR1B |= (1 << CS12);
+  TCCR1B &= ~(1 << CS11);
+  TCCR1B &= ~(1 << CS10);
+  TCNT1 = timer;
+  OCR1A = compare;
+  TIMSK1 = (1 << OCIE1A);
+  sei();
 }
 
-void loop() {
-  if (motionDetected) {
-    float temp = getTemperature();
-    Serial.println("Motion detected!");
-    Serial.print("Temperature: ");
-    Serial.print(temp);
-    Serial.println(" C");
-    digitalWrite(ledPin, HIGH);
-    delay(1000);
-    motionDetected = false;
-  } else if (tiltDetected) {
-    Serial.println("Tilt detected!");
-    digitalWrite(ledPin, HIGH);
-    delay(1000);
-    tiltDetected = false;
-  } else {
-    digitalWrite(ledPin, LOW);
+  void loop() {
+}
+
+ISR(PCINT2_vect) {
+  if (prepir != digitalRead(pirPin)) {
+    digitalWrite(led3, digitalRead(pirPin));
+    Serial.println("PIR sensor detected motion");
+    prepir = digitalRead(pirPin);
   }
-  PORTB ^= (1 << ledPin);
-  delay(500);
+  if (prebutton != digitalRead(buttonPin)) {
+    if (digitalRead(led2) == LOW) {
+      digitalWrite(led2, HIGH);
+    } 
+    else {
+      digitalWrite(led2, LOW);
+    }
+    Serial.println("Button pressed");
+    prebutton = digitalRead(buttonPin);
+  }
 }
 
-void motionDetectedIsTrue() {
-  motionDetected = true;
-}
-float getTemperature() {
-  int sensorValue = analogRead(tmp36Pin);
-  float voltage = sensorValue * (5.0 / 1023.0);
-  float temperature = (voltage - 0.5) * 100.0;
-
-  return temperature;
-}
-void tiltDetectedIsTrue() {
-  tiltDetected = true;
+ISR(TIMER1_COMPA_vect) {
+  TCNT1 = timer;
+  digitalWrite(led1, digitalRead(led1) ^ 1);
 }
